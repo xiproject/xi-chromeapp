295516834f09b90514330b1b71fc43e346f57400
Mihir Rege <mihirrege@gmail.com>
2015-03-22 09:21:54 +0530

    Make recognition more robust

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,15 +2,26 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var h1 = document.getElementsByTagName('h1');
-    if (h1.length > 0) {
-        h1[0].innerText = h1[0].innerText + ' \'Allo';
+    var h2 = document.getElementsByTagName('h2');
+    if (h2.length > 0) {
+        h2[0].innerText = h2[0].innerText + ' \'Allo';
     }
     var sr = new webkitSpeechRecognition();
     sr.continuous = true; 
     sr.interimResults = true;
     sr.lang='en-IN';
+    sr.onerror = function(event) {
+        console.log(event.error);
+    };
 
+    sr.onstart = function() {
+        console.log('Recognition started');
+
+    };
+    sr.onend = function() {
+        console.log('Recognition ended');
+        sr.start();
+    };
     sr.onresult = function(e) {
         var finalTranscript = '';
         var interimTranscript = '';
@@ -20,14 +31,20 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 interimTranscript += event.results[i][0].transcript;
             }
+        } 
+        for (var i = 0; i < event.results.length; ++i) {
+            var str;
+            if (i === event.resultIndex) {
+                str = 'result index';
+            }
+            console.log(event.results[i][0].transcript, event.results[i].isFinal, str);
         }
-        h1[0].innerText = finalTranscript;
+        h2[0].innerText = finalTranscript + interimTranscript;
         if (finalTranscript) {
             $.get("http://localhost:9876", {'stt': finalTranscript}, function(response) {
                 console.log("response: ", response);
 	    });
         }
-
     };
     sr.start();
 }, false);
